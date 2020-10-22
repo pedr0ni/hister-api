573a327b8e59530b83ab4921de82e5e25f6c86fb
Matheus Pedroni <matheus.pedronirodrigues-ext@ab-inbev.com>
2020-10-22 16:26:46 -0300

    Fixed tests

--- src/Factory.ts
+++ src/Factory.ts
@@ -2,21 +2,24 @@ import faker from 'faker'
 import Category from '../src/models/Category'
 import User, { IUser } from '../src/models/User'
 import app from '../src/app'
+import { IOrderBook } from './models/Order'
 
 export interface IUserParms {
     name?: string,
     email?: string,
     birth?: string,
+    cart?: Array<IOrderBook>,
     password?: string
 }
 
 export default {
     async getUser(params: IUserParms = {}) {
         return await User.create({
-            name: params.name ? params.name : faker.name.findName(),
-            email: params.email ? params.email : faker.internet.email(),
-            birth: params.birth ? params.birth : faker.date.past().toString(),
-            password: params.password ? params.password : faker.internet.password(),
+            name: params.name ?? faker.name.findName(),
+            email: params.email ?? faker.internet.email(),
+            birth: params.birth ?? faker.date.past().toString(),
+            password: params.password ?? faker.internet.password(),
+            cart: params.cart ?? [],
             credit: (Math.random() * 500).toFixed(2)
         })
     },
