87602b4da135e13966fd21e2c4ccc54f01edf757
Matheus Pedroni <matheus.pedronirodrigues-ext@ab-inbev.com>
2020-12-02 20:18:35 -0300

    Added dashboardcontroller

---
+++ src/controllers/DashboardController.ts
@@ -0,0 +1,27 @@
+import express from 'express'
+import User from '../models/User'
+import AuthenticationMiddleware from '../middlewares/AuthenticationMiddleware'
+import Book from '../models/Book'
+import Order from '../models/Order'
+
+const router = express.Router()
+
+router.use(AuthenticationMiddleware)
+
+router.get('/', async (req, res) => {
+    try {
+
+        const users = await User.count({})
+        const books = await Book.count({})
+        const orders = await Order.count({})
+
+        return res.json({
+            users, books, orders
+        })
+
+    } catch (exception) {
+        res.status(500).json(exception)
+    }
+})
+
+export default router
